597e1d79fc0dbf99947bb21b33cfae19f9c94dba
Shayan Shojaei <68788931+shayan-shojaei@users.noreply.github.com>
2025-03-04 10:18:48 +0330

    feat: query params validation in route handlers

--- src/common/helpers/handleRoute.helper.ts
+++ src/common/helpers/handleRoute.helper.ts
@@ -2,13 +2,21 @@ import { ClassConstructor } from 'class-transformer';
 import { NextFunction, Request, Response } from 'express';
 import { ValidationHelper } from './validation.helper';
 
-export type HandleRouteOptions<BodyType extends object> = {
+export type HandleRouteOptions<
+    BodyType extends object,
+    QueryType extends Record<string, string>,
+> = {
     body?: ClassConstructor<BodyType>;
+    query?: ClassConstructor<QueryType>;
 };
 
-export const handleRoute = <T, BodyType extends object>(
+export const handleRoute = <
+    T,
+    BodyType extends object,
+    QueryType extends Record<string, string>,
+>(
     func: (req: Request, res: Response, next: NextFunction) => Promise<T>,
-    options?: HandleRouteOptions<BodyType>,
+    options?: HandleRouteOptions<BodyType, QueryType>,
 ) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
@@ -16,6 +24,13 @@ export const handleRoute = <T, BodyType extends object>(
                 req.body = await ValidationHelper.validate(
                     req.body,
                     options.body,
+                );
+            }
+
+            if (options?.query) {
+                req.query = await ValidationHelper.validate(
+                    req.query,
+                    options.query,
                 );
             }
 
